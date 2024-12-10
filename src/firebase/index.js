086044ece3ed6da6e46185d52d0ed08731b321ab
Richard Li <richard.li12@student.tdsb.on.ca>
2024-12-10 11:31:17 -0500

    added firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCKb_k__t50WDh55AsXAQz61hdXPRA5u5w",
+    authDomain: "summative-9360c.firebaseapp.com",
+    projectId: "summative-9360c",
+    storageBucket: "summative-9360c.firebasestorage.app",
+    messagingSenderId: "283139748870",
+    appId: "1:283139748870:web:20ba98e07b018507393f87"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
